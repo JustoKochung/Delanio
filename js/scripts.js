eeac29214f922a4b8d64eb9e9d6bdb7ab3e9e049
JustoKochung <kochungjusto26@gmail.com>
2020-07-31 18:47:56 +0300

    Add toggle functions to what we do section

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $(".design").click(function(){
+    $(".p-text").toggle();
+  });
+  $(".design1").click(function(){
+    $(".p-text1").toggle();
+  });
+  $(".design2").click(function(){
+    $(".p-text2").toggle();
+  });
+});
